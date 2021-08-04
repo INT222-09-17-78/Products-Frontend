46b85740b5bb72f4100948574b8d5a9987cfbbaf
62130500017 <jirawath.jiw@mail.kmutt.ac.th>
2021-08-05 00:48:42 +0700

    set up environment for project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './styles/output.css'
+import Login from './components/login.js'
+function App() {
+  return (
+    <div className="App bg-cyan-blue w-screen h-screen flex justify-center">
+       <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login.js
@@ -0,0 +1,9 @@
+
+function Login() {
+    return (
+      <div className="login bg-snow w-52 h-32">
+          
+      </div>
+    );
+}
+export default Login
